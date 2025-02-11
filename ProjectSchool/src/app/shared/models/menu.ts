cd72da530319a24bda8cdfca1c32df21443d4c82
Giovani Silva <giovani-silva1@live.com>
2025-02-11 17:05:18 -0300

    criado o MenuItem e MenuItemList e os modulos

---
+++ ProjectSchool/src/app/shared/models/menu.ts
@@ -0,0 +1,29 @@
+import { MenuItem } from './menuItem';
+
+export const menuItems: MenuItem[] = [
+  {
+    link: '/dashboard',
+    icon: 'fa-chart-line',
+    label: 'Dashboard',
+  },
+  {
+    link: '/courses',
+    icon: 'fa-grauation-cap',
+    label: 'Cursos',
+  },
+  {
+    link: '/teachers',
+    icon: 'fa-person-chaçlboard',
+    label: 'Professores',
+  },
+  {
+    link: '/students',
+    icon: 'fa-charkboard-user',
+    label: 'Alunos',
+  },
+  {
+    link: '/support',
+    icon: 'fa-headset',
+    label: 'Suporte',
+  },
+];
